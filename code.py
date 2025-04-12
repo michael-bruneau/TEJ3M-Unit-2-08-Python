101a534360484620cad42159d4e9660966ace38c
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2025-04-12 11:05:35 -0400

    fixed comments

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # Created by: Michael Bruneau
 # Created on: April 2025
 #
-# This module is a Raspberry Pi Pico program that turns Micro Servo if an object gets within 50cm of the Ultrasonic Distance Sensor
+# This module is a Raspberry Pi Pico program causes a micro servo to spin 180 degrees when potentiometer resistance is 1MΩ and 0 degrees at 0Ω
 
 
 import time
@@ -14,8 +14,8 @@ import analogio
 
 
 # variables and constants
-servo_delay = 2
 UNIT_ANGLE = 0.0027
+servo_delay = 2
 servo_angle = 0
 biggest_angle = 0
 wiper_output = 0
@@ -31,10 +31,10 @@ my_servo = servo.Servo(pwm)
 
 # loop
 while True:
-    # get wiper output
+    # find wiper output
     wiper_output = potentiometer.value
 
-    # calculates angle
+    # calculates angle based on wiper output
     servo_angle = wiper_output * UNIT_ANGLE
 
     # turns servo
